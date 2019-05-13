85d6fc845ec1d43e53dd93e409be773437e0e78d
Jen Chow <jennego@gmail.com>
2019-05-12 17:20:54 -0700

    Got create form to work for quote and author. Catergories don't work - amy need to go the many-to-many taggings route

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,18 @@
 class QuotesController < ApplicationController
+
+    def new
+        @quote = Quote.new
+        @quote.categories.build
+        @quote.build_author
+    end
+
     def create 
+        @quote = Quote.new quote_params
+      if @quote.save
+        redirect_to root_path
+      else
+        render :new
+      end
     end
 
     def index 
@@ -8,6 +21,14 @@ class QuotesController < ApplicationController
 
     def show 
         @quote = Quote.find params[:id]
+    end
+
+    private 
+
+    def quote_params
+ 
+    params.require(:quote).permit(:quotetext, author_attributes: [:id, :author_first, :author_last], category_attributes: [:id, :category])
+    
     end
 
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,8 @@
 class Quote < ApplicationRecord
     has_many :categories 
     belongs_to :author 
+
+    accepts_nested_attributes_for :categories
+    accepts_nested_attributes_for :author
+
 end
